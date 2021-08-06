a977460b220d6b1ddcff13f98fec18c870c7888f
dondakeshimo <went.went.takkun135@gmail.com>
2021-08-06 21:33:45 +0900

    feat: add tag manager

--- nextjs/blog-starter-typescript-app/pages/_app.tsx
+++ nextjs/blog-starter-typescript-app/pages/_app.tsx
@@ -1,6 +1,20 @@
 import { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
 import '../styles/index.scss'
 
+const tagManagerArgs = {
+  gtmId: `${process.env.GTM_ID}`,
+  dataLayerName: 'dataLayer',
+  dataLayer: {
+    platform: 'nextjs',
+  }
+}
+
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+
   return <Component {...pageProps} />
 }
