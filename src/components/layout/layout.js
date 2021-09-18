15c1bf4a3016ba35278e2d57c4dedd18eb5a4dfc
dondakeshimo <went.went.takkun135@gmail.com>
2021-09-18 12:22:45 +0900

    feat: redirect new blog

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -6,10 +6,19 @@ import Header from './header'
 import Footer from './footer'
 import '../../style/global.less'
 import style from './layout.module.less'
+import { Link } from 'gatsby'
 
 const Layout = ({ children, title }) => (
   <>
     <Header />
+    <div className={style.container}>
+      <Link to="https://dondakeshimo.com">
+        <div className={style.messagebox}>
+          <h1>移行しました</h1>
+          <h2> <a className={style.underline}>dondakeshimo.com</a> をご参照ください</h2>
+        </div>
+      </Link>
+    </div>
     <div className={style.container}>
       {title ? (
         <div className={style.title}>
